00898679619354fbe0adf4369b2d044a65b90e4e
Túlio Cota <tuliocota@gmail.com>
2019-01-06 18:17:31 -0200

    Projeto criado

---
+++ src/main/java/br/com/tuliocota/cursomc/CursomcApplication.java
@@ -0,0 +1,14 @@
+package br.com.tuliocota.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
+
