13ffef3c610e0309f5ff5add69bf831139c5e3e2
Túlio Cota <tuliocota@gmail.com>
2019-01-08 20:21:28 -0200

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/tuliocota/cursomc/CursomcApplication.java
+++ src/main/java/br/com/tuliocota/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import br.com.tuliocota.cursomc.domain.Cidade;
 import br.com.tuliocota.cursomc.domain.Cliente;
 import br.com.tuliocota.cursomc.domain.Endereco;
 import br.com.tuliocota.cursomc.domain.Estado;
+import br.com.tuliocota.cursomc.domain.ItemPedido;
 import br.com.tuliocota.cursomc.domain.Pagamento;
 import br.com.tuliocota.cursomc.domain.PagamentoComBoleto;
 import br.com.tuliocota.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.tuliocota.cursomc.repositories.CidadeRepository;
 import br.com.tuliocota.cursomc.repositories.ClienteRepository;
 import br.com.tuliocota.cursomc.repositories.EnderecoRepository;
 import br.com.tuliocota.cursomc.repositories.EstadoRepository;
+import br.com.tuliocota.cursomc.repositories.ItemPedidoRepository;
 import br.com.tuliocota.cursomc.repositories.PagamentoRepository;
 import br.com.tuliocota.cursomc.repositories.PedidoRepository;
 import br.com.tuliocota.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -117,6 +122,19 @@ public class CursomcApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.0);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.0);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.0);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
--- src/main/java/br/com/tuliocota/cursomc/domain/Produto.java
+++ src/main/java/br/com/tuliocota/cursomc/domain/Produto.java
@@ -2,7 +2,10 @@ package br.com.tuliocota.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -24,11 +28,14 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
 	@JsonBackReference
 	@ManyToMany()
 	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 
@@ -39,6 +46,10 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return itens.stream().map(item -> item.getPedido()).collect(Collectors.toList());
 	}
 
 	public Integer getId() {
@@ -71,6 +82,14 @@ public class Produto implements Serializable {
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
