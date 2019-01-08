4b29fd9d3e0abb23a7c1683ca285631bbd158a7c
Túlio Cota <tuliocota@gmail.com>
2019-01-08 20:05:59 -0200

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/tuliocota/cursomc/CursomcApplication.java
+++ src/main/java/br/com/tuliocota/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package br.com.tuliocota.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.tuliocota.cursomc.domain.Cidade;
 import br.com.tuliocota.cursomc.domain.Cliente;
 import br.com.tuliocota.cursomc.domain.Endereco;
 import br.com.tuliocota.cursomc.domain.Estado;
+import br.com.tuliocota.cursomc.domain.Pagamento;
+import br.com.tuliocota.cursomc.domain.PagamentoComBoleto;
+import br.com.tuliocota.cursomc.domain.PagamentoComCartao;
+import br.com.tuliocota.cursomc.domain.Pedido;
 import br.com.tuliocota.cursomc.domain.Produto;
+import br.com.tuliocota.cursomc.domain.enums.EstadoPagamento;
 import br.com.tuliocota.cursomc.domain.enums.TipoCliente;
 import br.com.tuliocota.cursomc.repositories.CategoriaRepository;
 import br.com.tuliocota.cursomc.repositories.CidadeRepository;
 import br.com.tuliocota.cursomc.repositories.ClienteRepository;
 import br.com.tuliocota.cursomc.repositories.EnderecoRepository;
 import br.com.tuliocota.cursomc.repositories.EstadoRepository;
+import br.com.tuliocota.cursomc.repositories.PagamentoRepository;
+import br.com.tuliocota.cursomc.repositories.PedidoRepository;
 import br.com.tuliocota.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -32,15 +40,21 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -77,19 +91,33 @@ public class CursomcApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(e1, e2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
+
 		Endereco end1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
+
 		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(end1, end2));
-		
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, end1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, end2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
 	}
 
 }
--- src/main/java/br/com/tuliocota/cursomc/domain/Cliente.java
+++ src/main/java/br/com/tuliocota/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.tuliocota.cursomc.domain.enums.TipoCliente;
@@ -36,6 +37,10 @@ public class Cliente implements Serializable {
 	@ElementCollection()
 	@CollectionTable(name = "telefone")
 	private Set<String> telefones = new HashSet<>();
+
+	@JsonBackReference
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 
@@ -106,6 +111,18 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public void setTipoCliente(Integer tipoCliente) {
+		this.tipoCliente = tipoCliente;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,7 +147,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
